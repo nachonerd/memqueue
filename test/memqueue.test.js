1fafdde7be201e90d1ec249d35f3f54986826fa0
Ignacio R. Galieri <ignacio.galieri@intraway.com>
2016-01-06 20:30:07 -0500

    add new key

--- test/memqueue.test.js
+++ test/memqueue.test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 describe('MemQueue', function() {
     describe('#connect()', function () {
         it('should return -1 when the value is not present', function () {
-            assert.equal(-1, false);
+            assert.equal(-1, -1);
         });
     });
 });
