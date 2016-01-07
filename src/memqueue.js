f96ddb612f9f691d8e5cb8a699012251aac69450
Ignacio R. Galieri <ignacio.galieri@intraway.com>
2016-01-06 23:07:56 -0500

    Adding some use cases and his code

---
+++ src/memqueue.js
@@ -0,0 +1,9 @@
+var Memcached = require('memcached');
+
+function MemQueue(key, locations, options){
+    if (key == undefined) {
+        throw new Error("Must set queue key");
+    }
+}
+
+module.exports = MemQueue;
--- test/memqueue.test.js
+++ test/memqueue.test.js
@@ -1,8 +1,31 @@
-var assert = require('assert');
+var assert = require('assert')
+    , Memcached = require('memcached')
+    , sinon = require('sinon')
+    , MemQueue = require('../');
+
 describe('MemQueue', function() {
-    describe('#connect()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal(-1, -1);
+    describe('#new', function () {
+        it("should throws exception when you forgets set key", function () {
+            var message = "";
+            try {
+                new MemQueue();
+            } catch (e) {
+                message = e;
+            }
+            assert.equal(new Error("Must set queue key").message, message.message);
+        });
+        it("should throws exception when couldn't create memcached object", function () {
+            var message = "";
+            var mem = new Memcached();
+            sinon.stub(mem, "set").onCall(0).throws(
+                new Error("Coundn't create memcached object")
+            );
+            try {
+                mem.set();
+            } catch (e) {
+                message = e;
+            }
+            assert.equal(new Error("Coundn't create memcached object").message, message.message);
         });
     });
 });
