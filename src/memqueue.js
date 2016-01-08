00415abc7e431054d95312fe2f8737d9dabac4c0
Ignacio R. Galieri <ignacio.galieri@intraway.com>
2016-01-07 19:21:15 -0500

    Add some constructor use case his test and code

--- src/memqueue.js
+++ src/memqueue.js
@@ -4,6 +4,9 @@ function MemQueue(key, locations, options){
     if (key === undefined) {
         throw new Error("Must set queue key");
     }
+    if (!(typeof key == 'string' || typeof key == 'number')) {
+        throw new Error("The key parameter must be number or string");
+    }
 }
 
 module.exports = MemQueue;
--- test/memqueue.test.js
+++ test/memqueue.test.js
@@ -4,8 +4,8 @@ var assert = require('assert')
     , MemQueue = require('../');
 
 describe('MemQueue', function() {
-    describe('#new', function () {
-        it("should throws exception when you forgets set key", function () {
+    describe('#constructor', function () {
+        it("should throws exception when you forgets set key.", function () {
             var message = "";
             try {
                 new MemQueue();
@@ -14,7 +14,61 @@ describe('MemQueue', function() {
             }
             assert.equal(new Error("Must set queue key").message, message.message);
         });
-        it("should throws exception when couldn't create memcached object", function () {
+        it("should throws exception when key parameters is false.", function () {
+            var message = "";
+            try {
+                new MemQueue(false);
+            } catch (e) {
+                message = e;
+            }
+            assert.equal(new Error("The key parameter must be number or string").message, message.message);
+        });
+        it("should throws exception when key parameters is true.", function () {
+            var message = "";
+            try {
+                new MemQueue(true);
+            } catch (e) {
+                message = e;
+            }
+            assert.equal(new Error("The key parameter must be number or string").message, message.message);
+        });
+        it("should throws exception when key parameters is [].", function () {
+            var message = "";
+            try {
+                new MemQueue([]);
+            } catch (e) {
+                message = e;
+            }
+            assert.equal(new Error("The key parameter must be number or string").message, message.message);
+        });
+        it("should throws exception when key parameters is [1,'aa'].", function () {
+            var message = "";
+            try {
+                new MemQueue([1,'aa']);
+            } catch (e) {
+                message = e;
+            }
+            assert.equal(new Error("The key parameter must be number or string").message, message.message);
+        });
+        it("should throws exception when key parameters is {}.", function () {
+            var message = "";
+            try {
+                new MemQueue({});
+            } catch (e) {
+                message = e;
+            }
+            assert.equal(new Error("The key parameter must be number or string").message, message.message);
+        });
+        it("should throws exception when key parameters is {'field':true}.", function () {
+            var message = "";
+            try {
+                new MemQueue({'field':true});
+            } catch (e) {
+                message = e;
+            }
+            assert.equal(new Error("The key parameter must be number or string").message, message.message);
+        });
+        it("should throws exception when couldn't create memcached object.", function () {
             var message = "";
             var mem = new Memcached();
             sinon.stub(mem, "set").onCall(0).throws(
